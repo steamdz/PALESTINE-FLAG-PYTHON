8b56dc2e5a026a32f580d486e238755ff4c47325
AYMEN DEV <68467119+steamdz@users.noreply.github.com>
2021-10-06 01:27:52 +0200

    commit intial

---
+++ aymen.py
@@ -0,0 +1,52 @@
+#AYMEN DEV 
+
+from turtle import *
+
+speed(1)
+setup(800,400)
+penup()
+goto(-400,200)
+pendown()
+
+#black recangle 
+color("black")
+begin_fill()
+forward(800)
+right(90)
+forward(133)
+right(90)
+forward(800)
+right(180)
+end_fill()
+
+#whie recangle 
+color("white")
+begin_fill()
+forward(800)
+right(90)
+forward(133)
+right(90)
+forward(800)
+right(180)
+end_fill()
+
+#green recangle 
+color("green")
+begin_fill()
+forward(800)
+right(90)
+forward(133)
+right(90)
+forward(800)
+right(180)
+end_fill()
+
+#red recangle 
+color("red")
+begin_fill()
+goto(-200,20)
+left(320)
+forward(-400)
+end_fill()
+
+mainloop()
